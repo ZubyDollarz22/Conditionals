1c6d900b0aaa392da904af3c1d34102570670f75
ZubyDollarz <zubydollarz22@gmail.com>
2024-11-07 22:22:45 +0100

    EightBall Break

---
+++ EightBall/app.js
@@ -0,0 +1,14 @@
+var userName = 'ZubyDollarz';
+userName === 'ZubyDollarz'?
+console.log(`Hello, ${userName}!`): 
+console.log('Hello!');
+
+var userQuestion = 'What is aim for the year?'
+console.log(userQuestion);
+
+var randomNumber = Math.floor(Math.random(4) * 8);
+var eightBall = '';
+switch(eightBall){
+  case 'It is certain':
+    console.log(eightBall);
+}
